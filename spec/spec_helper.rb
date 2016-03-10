943624ea505cac163a431796f0a0eb766b5ae89a
Kelton Manzanares <keltonmanzanares@gmail.com>
2016-03-10 07:06:46 -0700

    allowing code climate to report from tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,9 @@ require 'crowd_pay'
 ENV['CROWD_PAY_DOMAIN'] = 'https://test.crowdpay.com'
 ENV['CROWD_PAY_API_KEY'] = 'test'
 ENV['CROWD_PAY_PORTAL_KEY'] = 'test'
-ENV['CROWD_PAY_BY_PASS_VALIDATION'] = 'test'
+ENV['CROWD_PAY_BY_PASS'] = 'test'
+
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
